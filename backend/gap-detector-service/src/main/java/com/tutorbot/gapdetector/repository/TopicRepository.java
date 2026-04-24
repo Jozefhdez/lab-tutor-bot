e22f8c9216bd2c58e08cbc848621fb67b676f63c
Ana Elena Velasco García <143124370+Anaaveg17@users.noreply.github.com>
2026-04-23 19:01:58 -0600

    Created repository methods

--- backend/gap-detector-service/src/main/java/com/tutorbot/gapdetector/repository/TopicRepository.java
+++ backend/gap-detector-service/src/main/java/com/tutorbot/gapdetector/repository/TopicRepository.java
@@ -1,10 +1,23 @@
-package com.tutorbot.gapdetector.repository;
+package com.tutorbot.evaluator.repository;
 
-import com.tutorbot.gapdetector.model.Topic;
-import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.tutorbot.evaluator.model.Topic;
 
 public interface TopicRepository extends JpaRepository<Topic, Long> {
 
-    List<Topic> findByCourseId(Long courseId);
+    @Query("SELECT t FROM Topic t WHERE t.id = :topicId AND t.active = true")
+    Optional<Topic> findActiveById(@Param("topicId") Long topicId);
+
+    @Query("SELECT " +
+            "CASE WHEN t.skill.active = true " +
+            "THEN true ELSE false " +
+            "END " +
+            "FROM Topic t WHERE t.id = :topicId " +
+            "AND t.skill.active = true")
+    Boolean isSkillActiveByTopicId(@Param("topicId") Long topicId);
 }
